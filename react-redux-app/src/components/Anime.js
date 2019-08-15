6c9933a20a34f77d5b8017c4fc75bcf29bcbcbea
Kenya-a <Kenya-a@users.noreply.github.com>
2019-08-15 18:36:43 -0500

    .

--- react-redux-app/src/components/Anime.js
+++ react-redux-app/src/components/Anime.js
@@ -5,6 +5,7 @@ const Anime = props => {
     return (
         <div>
             <h3>{props.el.title}</h3>
+            <img src = {props.el.image_url} alt = {props.el.name}/>
             <p>Anime Description</p>
         </div>
     )
